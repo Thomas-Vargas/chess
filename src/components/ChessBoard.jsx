bb9b9b4bb08c573490fd66d8ca10c5874f5b4752
Thomas Vargas <tavargas2012irc@gmail.com>
2023-05-31 23:16:46 -0500

    set up chess board and pieces, colors for each square still need to be corrected

--- src/components/ChessBoard.jsx
+++ src/components/ChessBoard.jsx
@@ -1,38 +1,79 @@
-import React from 'react';
-import Piece from './Piece';
+import React from "react";
+import Piece from "./Piece";
+import { Grid } from "@mui/material";
 
-const ChessBoard = ({ boardState }) => {
-    let board = boardState.board
-  const renderSquare = (square) => {
-    const piece = board[square];
+const ChessBoard = () => {
+  const boardState = {
+    board: {
+      a1: { piece: "rook", player: "white" },
+      b1: { piece: "knight", player: "white" },
+      c1: { piece: "bishop", player: "white" },
+      d1: { piece: "queen", player: "white" },
+      e1: { piece: "king", player: "white" },
+      f1: { piece: "bishop", player: "white" },
+      g1: { piece: "knight", player: "white" },
+      h1: { piece: "rook", player: "white" },
+      a2: { piece: "pawn", player: "white" },
+      b2: { piece: "pawn", player: "white" },
+      c2: { piece: "pawn", player: "white" },
+      d2: { piece: "pawn", player: "white" },
+      e2: { piece: "pawn", player: "white" },
+      f2: { piece: "pawn", player: "white" },
+      g2: { piece: "pawn", player: "white" },
+      h2: { piece: "pawn", player: "white" },
+      a8: { piece: "rook", player: "black" },
+      b8: { piece: "knight", player: "black" },
+      c8: { piece: "bishop", player: "black" },
+      d8: { piece: "queen", player: "black" },
+      e8: { piece: "king", player: "black" },
+      f8: { piece: "bishop", player: "black" },
+      g8: { piece: "knight", player: "black" },
+      h8: { piece: "rook", player: "black" },
+      a7: { piece: "pawn", player: "black" },
+      b7: { piece: "pawn", player: "black" },
+      c7: { piece: "pawn", player: "black" },
+      d7: { piece: "pawn", player: "black" },
+      e7: { piece: "pawn", player: "black" },
+      f7: { piece: "pawn", player: "black" },
+      g7: { piece: "pawn", player: "black" },
+      h7: { piece: "pawn", player: "black" },
+    },
+    currentPlayer: "white",
+  };
+
+  const renderSquare = (square, isDark) => {
+    const piece = boardState.board[square];
     // Check if the square is empty
     if (!piece) {
-      return <div className="square empty-square" />;
+      return <div className={`square ${isDark ? "dark" : "light"}-square`} />;
     }
-    // Render the piece component based on the piece type
-    // You can create separate components for each chess piece
-    // and pass the necessary props (e.g., piece type, player) to render the piece correctly.
-    return <Piece piece={piece} />;
+    // Render the Piece component and pass the necessary props
+    return <Piece piece={piece} color={piece.player} />;
   };
 
   const renderRow = (row) => {
-    const rowSquares = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-    return rowSquares.map((col) => {
+    const rowSquares = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    return rowSquares.map((col, index) => {
       const square = `${col}${row}`;
-      return <div key={square}>{renderSquare(square)}</div>;
+      const isDark = (index + row) % 2 === 0; // Check if the square is dark
+      return <div key={square}>{renderSquare(square, isDark)}</div>;
     });
   };
 
   const renderBoard = () => {
     const rows = [8, 7, 6, 5, 4, 3, 2, 1];
-    return rows.map((row) => (
-      <div key={row} className="board-row">
-        {renderRow(row)}
-      </div>
-    ));
+    return (
+      <Grid container direction="column" className="chess-board">
+        {rows.map((row, index) => (
+          <Grid container item key={row} className={`board-row ${index % 2 === 0 ? "dark" : "light"}-row`}>
+            {renderRow(row)}
+          </Grid>
+        ))}
+      </Grid>
+    );
   };
 
-  return <div className="chess-board">{renderBoard()}</div>;
+  return <div style={{ width: "484px" }}>{renderBoard()}</div>;
 };
 
 export default ChessBoard;
--- src/components/Piece.jsx
+++ src/components/Piece.jsx
@@ -1,30 +1,63 @@
-import React from 'react';
+import React from "react";
 
-const Piece = ({ piece }) => {
+const Piece = ({ piece, isDark, color }) => {
+    const pieceColor = color === "white" ? "white" : "black";
   const pieceStyles = {
-    // Define styles for each piece type
-    // You can customize the styles based on your design requirements
-    // Here, we're using simple text representations for the pieces
-    'rook': {
-      content: '♖',
+    white: {
+      king: {
+        content: <img src="/chess-pieces/white-king.png" alt="White King" />,
+      },
+      queen: {
+        content: <img src="/chess-pieces/white-queen.png" alt="White Queen" />,
+      },
+      rook: {
+        content: <img src="/chess-pieces/white-rook.png" alt="White Rook" />,
+      },
+      knight: {
+        content: (
+          <img src="/chess-pieces/white-knight.png" alt="White Knight" />
+        ),
+      },
+      bishop: {
+        content: (
+          <img src="/chess-pieces/white-bishop.png" alt="White Bishop" />
+        ),
+      },
+      pawn: {
+        content: <img src="/chess-pieces/white-pawn.png" alt="White Pawn" />,
+      },
     },
-    'knight': {
-      content: '♘',
+    black: {
+      king: {
+        content: <img src="/chess-pieces/black-king.png" alt="Black King" />,
+      },
+      queen: {
+        content: <img src="/chess-pieces/black-queen.png" alt="Black Queen" />,
+      },
+      rook: {
+        content: <img src="/chess-pieces/black-rook.png" alt="Black Rook" />,
+      },
+      knight: {
+        content: (
+          <img src="/chess-pieces/black-knight.png" alt="Black Knight" />
+        ),
+      },
+      bishop: {
+        content: (
+          <img src="/chess-pieces/black-bishop.png" alt="Black Bishop" />
+        ),
+      },
+      pawn: {
+        content: <img src="/chess-pieces/black-pawn.png" alt="Black Pawn" />,
+      },
     },
-    'bishop': {
-      content: '♗',
-    },
-    'pawn': {
-      content: '♙',
-    },
-    // ... other piece types
   };
 
-  const pieceStyle = pieceStyles[piece];
+  const pieceStyle = pieceStyles[piece.player][piece.piece];
 
   return (
-    <div className="piece">
-      {pieceStyle && <span>{pieceStyle.content}</span>}
+    <div className={`square piece ${isDark ? "dark" : "light"}-square`}>
+      <span className={`chess-piece ${pieceColor}`}>{pieceStyle.content}</span>
     </div>
   );
 };
