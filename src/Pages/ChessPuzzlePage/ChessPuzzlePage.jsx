2d9dce3d8c01fa94b84f514c9eed31904b664587
Thomas Vargas <tavargas2012irc@gmail.com>
2023-10-05 14:53:07 -0500

    added calculateEloChange function to chessPuzzlePage to be used for updating player elo in the future

--- src/Pages/ChessPuzzlePage/ChessPuzzlePage.jsx
+++ src/Pages/ChessPuzzlePage/ChessPuzzlePage.jsx
@@ -1,5 +1,21 @@
 
 const ChessPuzzlePage = () => {
+    
+    function calculateEloChange(playerElo, puzzleDifficulty, isSolved) {
+        const K = 16; // Adjust this based on your system's K-factor
+    
+        // Calculate expected outcome (E)
+        const E = 1 / (1 + Math.pow(10, (puzzleDifficulty - playerElo) / 400));
+    
+        // Calculate actual outcome (S)
+        const S = isSolved ? 1 : 0;
+    
+        // Calculate Elo change
+        const eloChange = K * (S - E);
+    
+        return Math.round(eloChange);
+    }
+      
     return (
         <div>
 
