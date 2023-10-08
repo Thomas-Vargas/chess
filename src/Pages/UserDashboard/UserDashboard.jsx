244f8e5325b479a6358e436db0527010cf48cb34
Thomas-Vargas <tavargas2012irc@gmail.com>
2023-10-07 21:01:26 -0500

    set up route for getting user data

--- src/Pages/UserDashboard/UserDashboard.jsx
+++ src/Pages/UserDashboard/UserDashboard.jsx
@@ -1,14 +1,37 @@
+import { useEffect, useState } from "react";
+import { Stack, TextField, Button, Typography } from "@mui/material";
+import { supabaseClient } from "../../config/supabaseClient";
 import { useAuth } from "../../components/AuthProvider/AuthProvider";
 
 const UserDashboard = () => {
-
+  const [userData, setUserData] = useState(null);
   const { user } = useAuth();
 
   console.log("user", user);
+  console.log("user data", userData);
+
+  const getUserData = async () => {
+    let { data: user_data, error } = await supabaseClient
+        .from('user_data')
+        .select("*")
+        .eq("userID", user.id)
+
+        if (user_data) {
+            setUserData(user_data[0]);
+        }
+  }
+
+  useEffect(() => {
+    if (user) {
+        getUserData();
+    }
+  }, [user]);
 
     return (
         <div>
-
+            <Stack direction="row" justifyContent="flex-start">
+                <Typography></Typography>
+            </Stack>
         </div>
     )
 }
